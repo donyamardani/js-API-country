0d224836748f59442c4b8c1ec758cdfb2c1d4983
donyamardani <zmardani0@gmail.com>
2025-10-04 23:46:53 +0330

    create html and css file
    create styles and js file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+const inpEl=document.querySelector('#inp');
+const btnEl=document.querySelector('#serch');
+const showEl=document.querySelector('#show-card');
+const showBorderEl=document.querySelector('.card-border');
+inpEl.addEventListener('change',async(e)=>
+{try{
+               const res=await fetch(`https://restcountries.com/v3.1/name/${e.target.value}?fullText=true`)
+               const data=await res.json()
+               showEl.innerHTML=`<div class='card'><img src=${data[0].flags.svg}></img><h2>${data[0].name.common}</h2></div> `
+    }
+    catch(error){alert(error)}
+})
