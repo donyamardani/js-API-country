6fe654d7f3811102e80046579636d3b25ec9ca61
donyamardani <zmardani0@gmail.com>
2025-10-05 01:56:18 +0330

    create fainally style and code
    create fainally style and code

--- js/script.js
+++ js/script.js
@@ -8,6 +8,11 @@ inpEl.addEventListener('change',async(e)=>
                const res=await fetch(`https://restcountries.com/v3.1/name/${e.target.value}?fullText=true`)
                const data=await res.json()
                showEl.innerHTML=`<div class='card'><img src=${data[0].flags.svg}></img><h2>${data[0].name.common}</h2></div> `
+               const borders=await fetch(`https://restcountries.com/v3.1/alpha?codes=${data[0].borders}`);
+               const bordersData=await borders.json();
+               bordersData.map(e=>{
+                showBorderEl.innerHTML+=`</br><div class='card-border'><img src=${e.flags.svg}></img><p>${e.name.common}</p></div>`
+               })
     }
     catch(error){alert(error)}
 })
